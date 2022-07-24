690624cb1d7f8f653d85568a70304b481a723c5a
jangwooooo <s22018@gsm.hs.kr>
2022-07-24 23:07:44 +0900

    4.3 Events part Two

--- js/greeting.js
+++ js/greeting.js
@@ -1,9 +1,16 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
+const link = document.querySelector("a");
 
 function loginSubmit(event){
     event.preventDefault();
     console.log(loginInput.value);
 }
 
+function linkClick(event){
+    event.preventDefault();
+    console.dir(event);
+}
+
 loginForm.addEventListener("submit", loginSubmit);
+link.addEventListener("click", linkClick);
