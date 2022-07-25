642b46584cf1edabcd08fe30eedc7b537d086afd
jangwooooo <s22018@gsm.hs.kr>
2022-07-25 09:43:48 +0900

    4.4 Getting Username

--- js/greeting.js
+++ js/greeting.js
@@ -1,16 +1,15 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
-const link = document.querySelector("a");
+const greeting = document.querySelector("#greeting");
+
+const HIDDEN_CLASSNAME = "hidden"
 
 function loginSubmit(event){
     event.preventDefault();
-    console.log(loginInput.value);
-}
-
-function linkClick(event){
-    event.preventDefault();
-    console.dir(event);
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME)
 }
 
 loginForm.addEventListener("submit", loginSubmit);
-link.addEventListener("click", linkClick);
